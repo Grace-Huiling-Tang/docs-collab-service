fbdad6892bc6926cc1e80f33cc4465f6d848d0e0
Huiling <36881119+Grace-Huiling-Tang@users.noreply.github.com>
2026-02-26 18:32:47 -0500

    Fix V2 Flyway migration

--- src/main/resources/db/migration/V2__create_projects.sql
+++ src/main/resources/db/migration/V2__create_projects.sql
@@ -1,6 +1,8 @@
 CREATE TABLE projects (
   id uuid PRIMARY KEY,
   name text NOT NULL,
-  create_by uuid NOT NULL FOREIGN KEY,
-  created_at timestamptz NOT NULL DEFAULT now()
+  create_by uuid NOT NULL,
+  created_at timestamptz NOT NULL DEFAULT now(),
+
+   FOREIGN KEY (create_by) REFERENCES users(id) ON DELETE CASCADE
 );
