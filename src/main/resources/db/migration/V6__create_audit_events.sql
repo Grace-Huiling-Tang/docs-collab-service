2632da497cc0adfd763284efa4a0fe291297f777
jayesh12345-p <john110318@gmail.com>
2026-02-27 21:34:18 -0500

    Final

--- src/main/resources/db/migration/V6__create_audit_events.sql
+++ src/main/resources/db/migration/V6__create_audit_events.sql
@@ -4,9 +4,11 @@ CREATE TABLE audit_events (
   version_id uuid, 
   -- create, delete are for version events, member_added, member_removed, role_change are for membership events, udpate is for project info update like description, name, etc, In practice, updated content will not be stored. Just print, project setting updated.
   event_type text NOT NULL CHECK (
-  (event_type IN ('created','updated','deleted') AND version_id IS NOT NULL AND actor_user_id IS NOT NULL)
+  (event_type IN ('created','deleted') AND version_id IS NOT NULL AND actor_user_id IS NOT NULL AND project_id IS NOT NULL AND target_user_id IS NULL)
   OR
-  (event_type IN ('member_added','member_removed','role_change') AND project_id IS NOT NULL AND target_user_id IS NOT NULL)
+  (event_type = 'updated' AND version_id IS NULL AND actor_user_id IS NOT NULL AND project_id IS NOT NULL AND target_user_id IS NULL)
+  OR
+  (event_type IN ('member_added','member_removed','role_change') AND project_id IS NOT NULL AND target_user_id IS NOT NULL AND version_id IS NULL AND actor_user_id IS NOT NULL)
 ),
  -- Type of event
   actor_user_id uuid NOT NULL, -- Foreign key to users table
